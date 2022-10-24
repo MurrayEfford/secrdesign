921bda93f46c758b141a92e1262875467b7ff419
MurrayEfford <MurrayEfford@users.noreply.github.com>
2022-10-24 14:15:31 +1300

    include En2

--- tests/testthat/test-nrm.R
+++ tests/testthat/test-nrm.R
@@ -21,8 +21,10 @@ nrm.p <- Enrm(D = 5, trp, msk, list(lambda0 = 0.2, sigma = 20), 5)
 # En       Er       Em 
 # 20.42885 56.14689 49.64859 
 
-Qpm.m <- Qpm(D = 5, trm, msk, list(lambda0 = 0.2, sigma = 20), 5)
 Qpm.p <- Qpm(D = 5, trp, msk, list(lambda0 = 0.2, sigma = 20), 5)
+
+en2.m <- En2(D = 5, trm, msk, list(lambda0 = 0.2, sigma = 20), 5)
+en2.p <- En2(D = 5, trp, msk, list(lambda0 = 0.2, sigma = 20), 5)
 
 test_that("Enrm correct for 'multi'", {
     expect_equal(unname(nrm.m), c(20.42885, 32.59759, 28.62447), tolerance = 1e-4)
@@ -35,4 +37,15 @@ test_that("Enrm correct for 'proximity'", {
 test_that("Qpm correct for 'proximity'", {
     expect_equal(unname(Qpm.p), c(0.3817752, 0.2816757), tolerance = 1e-4)
 })
+
+test_that("En2 correct for 'multi'", {
+    expect_equal(unname(en2.m), c(20.42885, 13.80840), tolerance = 1e-4)
+})
+
+test_that("En2 correct for 'proximity'", {
+    expect_equal(unname(en2.p), c(20.42885, 15.07251), tolerance = 1e-4)
+})
+
+
+
 ###############################################################################
