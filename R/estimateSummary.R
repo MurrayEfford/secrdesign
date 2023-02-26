a0f8a4d229262497331b19b8ae39b4a36011abd7
MurrayEfford <MurrayEfford@users.noreply.github.com>
2023-02-26 17:05:13 +1300

    estimateSummary() tweaks

--- R/estimateSummary.R
+++ R/estimateSummary.R
@@ -1,3 +1,8 @@
+###############################################################################
+## package 'secrdesign'
+## estimateSummary.R
+## 2023-02-25
+###############################################################################
 
 # Convert output (nested list of estimate tables) to array
 # with dimensions (Parameter, statistic, Group, Scenario, Replicate)
@@ -17,6 +22,13 @@ estimateArray <- function (object) {
         ngrp <- 1
         groupnames <- 1
     }
+    # count actual tables per replicate of each scenario
+    ntab <- sapply(object$output, 
+        function(x) if (is.data.frame(x[[1]])) 1 else length(x[[1]]))
+    if (length(table(ntab)) > 1) 
+        stop("varying number of estimatetables per scenario")
+    if (ntab[1] > ngrp)
+        warning("multiple tables per group")
     num <- sapply(typical, is.numeric)
     if (inherits(object, 'estimatetables')) {
         values <- unlist(object$output)
@@ -36,22 +48,24 @@ estimateArray <- function (object) {
     }
     else stop("unsuitable data; expecting estimatetables from secrdesign")
 }
+###############################################################################
 
 # Direct summary of output from run.scenarios
 estimateSummary <- function (object, parameter = 'D', 
-    statistics = c('n', 'estimate', 'SE.estimate', 'RB', 'seRB', 
-        'RSE', 'RMSE', 'COV'), true, validrange = c(0, Inf), 
+    statistics = c('true', 'nvalid', 'EST', 'seEST', 'RB', 
+        'seRB', 'RSE', 'RMSE', 'rRMSE', 'COV'), true, validrange = c(0, Inf), 
     checkfields = c('estimate','SE.estimate'),
-    format = c('list', 'data.frame')) {
+    format = c('list', 'data.frame'), cols = 1:2) {
     
-    format <- match.arg(format)
+    arr <- estimateArray(object)
     
     if (missing(true)) {
-        true <- object$scenarios[,parameter]
+        # assumes groups, if present, are used in model
+        # otherwise incompatible
+        true <- object$scenarios[,parameter] 
     }
-    arr <- estimateArray(object)
-
-    if (length(true) != prod(dim(arr)[3:4])) stop ("incongruent 'true'")
+    if (length(true) != prod(dim(arr)[3:4])) stop ("incompatible 'true' vector")
+    
     # check for out-of-range estimates
     validate <- function(x) {
         x1 <- as.numeric(x[checkfields])
@@ -65,20 +79,22 @@ estimateSummary <- function (object, parameter = 'D',
     else
         arr <- aperm(apply(arr, c(1,3,4,5), validate), c(2,1,3,4,5))
     
-    # subsets of data
+    # working subsets of data
     estcol <- c('estimate','SE.estimate')
+    # switch for coef()
     if (all(c('beta','SE.beta') %in% dimnames(arr)[[2]])) {
-        estcol <- c('beta','SE.beta') # switch for coef()
+        estcol <- c('beta','SE.beta') 
     }
     parm <- arr[parameter,estcol[1],,,, drop = FALSE]
     parmse <- arr[parameter,estcol,,,, drop = FALSE]
     parmcl <- arr[parameter, c('lcl','ucl'),,,, drop = FALSE]
     
     se <- function (x) sd(x, na.rm = TRUE) / sum(!is.na(x))^0.5
-    n <- apply(parm, 3:4, function(x) sum(!is.na(x)))
+    nvalid <- apply(parm, 3:4, function(x) sum(!is.na(x)))
     EST <- apply(parm, 3:4, mean, na.rm = TRUE)
     seEST <- apply(parm, 3:4, se)
     
+    truemat <- array(true, dim=dim(arr)[3:4], dimnames = dimnames(arr)[3:4])
     # relative bias
     rb <- function (x, true) (x-true)/true
     RB <- sweep(parm, MARGIN = 3:4, STATS = true, FUN = rb)
@@ -94,19 +110,53 @@ estimateSummary <- function (object, parameter = 'D',
     err <- function (x, true) (x-true)
     rms <- function (x) sqrt(mean(x^2, na.rm = TRUE))
     ERR <- sweep(parm, MARGIN = 3:4, STATS = true, FUN = err)
-    RMSE <- apply(ERR, 3:4, rms) / true
+    RMSE <- apply(ERR, 3:4, rms)
+    rRMSE <- RMSE / true
     
     # coverage of CI
-    inrange <- function(x) (x[1,1,,,drop=FALSE] < true) & (x[1,2,,,drop=FALSE] > true)
+    inrange <- function(x) (x[1,'lcl',,,drop=FALSE] < true) & (x[1,'ucl',,,drop=FALSE] > true)
     OK <- apply(parmcl, 5, inrange)
     dim(OK) <- dim(parmcl)[3:5]
-    COV <- apply(OK, 1, mean, na.rm = TRUE)
-    COV <- array(COV, dim = dim(parm)[3:4], dimnames = dimnames(parm)[3:4])
+    COV <- apply(OK, 1:2, mean, na.rm = TRUE)
+    COV <- array(COV, dim = dim(parmcl)[3:4], dimnames = dimnames(parm)[3:4])
     
-    out <- list(n = n, estimate = EST, SE.estimate = seEST, RB = mnRB, 
-        seRB = seRB, RSE = RSE, RMSE = RMSE, COV = COV)[statistics]
-    if (format == 'list') 
-        out
-    else
-        do.call(data.frame, lapply(out, as.numeric))
+    # assemble output, discarding unwanted summaries
+    out <- list(
+        true = truemat, 
+        nvalid = nvalid, 
+        EST = EST, 
+        seEST = seEST, 
+        RB = mnRB, 
+        seRB = seRB, 
+        RSE = RSE, 
+        RMSE = RMSE, 
+        rRMSE = rRMSE, 
+        COV = COV)[statistics]
+    if (names(out)[1] == 'true') names(out)[1] <- paste0("true.", parameter)
+    
+    # optionally recast output list as data.frame
+    format <- match.arg(format)
+    if (format == 'data.frame') {
+        out  <- do.call(data.frame, lapply(out, as.numeric))
+        # pre-pend columns from object$scenarios
+        ngrp <- max(1, length(unique(object$scenarios$group)))
+        if (dim(arr)[3]>1 && ngrp>1) {
+            # valid groups defined in scenarios df
+            rows <- 1:nrow(object$scenarios)   
+        }
+        else {
+            # groups from multi-session, etc.
+            if (dim(arr)[3]>1) {
+                rows <- rep(unique(object$scenarios$scenario), each = ngrp)
+            }
+            else  {
+                rows <- 1:nrow(out)
+            }
+            rows <- match(rows, object$scenarios$scenario)
+        }
+        scen <- object$scenarios[rows,cols, drop = FALSE]
+        if (!is.null(cols)) out <- cbind(scen, out)
+    }
+    
+    out
 }
